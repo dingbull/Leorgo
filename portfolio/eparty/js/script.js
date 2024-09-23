316893802c2b50a44c5c9d915dd8f7331b99234c
Rolan Ho <116419895+dingbull@users.noreply.github.com>
2024-09-22 23:57:08 -0600

    updating

---
+++ portfolio/eparty/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 300,
+        itemMargin: 20
+    });
+
+});
